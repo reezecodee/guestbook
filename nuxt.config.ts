b5ea24a18f95e946483a878fc2921405d8501d80
Reeze <azfaalharits25@gmail.com>
2025-06-14 22:00:27 +0700

    feat: tailwind css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
-  devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/test-utils']
-})
+  compatibilityDate: "2025-05-15",
+  devtools: { enabled: false },
+  modules: ["@nuxt/eslint", "@nuxt/test-utils"],
+  css: ['~/assets/css/main.css'],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1 class="text-red-500">{{ message }}</h1>
+    </div>
+</template>
+
+<script setup>
+const { data, pending, error } = await useFetch('/api/guestbook')
+
+const message = computed(() => {
+    if (pending.value) return 'Loading...'
+    if (error.value) return 'Error'
+    return data.value?.message
+})
+
+useHead({
+  title: 'Login | Guestbook App',
+  meta: [
+    { name: 'description', content: 'Halaman login untuk aplikasi guestbook' }
+  ]
+})
+</script>
+
+
+<style scoped></style>
